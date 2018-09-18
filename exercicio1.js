140fb6a093d6c6abdeb9a9a5bfcc6d8f7c377932
Aluno <aluno>
2018-09-18 12:06:20 -0300

    exercício 1.1 e 1.2

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,21 @@
 // Faça o exercício dos parágrafos aqui
 // Este arquivo ainda não está incluído no arquivo HTML
+
+//1.1
+// const elementos = document.querySelectorAll('.botao-expandir-retrair');
+
+// for(let e of elementos){
+// 	e.addEventListener('click', function(key){
+// 		alert('Cuidado !');
+// 	});
+// }
+
+//1.2
+const elementos = document.querySelectorAll('.botao-expandir-retrair');
+
+for(let e of elementos){
+	e.addEventListener('click', function(evt){
+		let el = evt.currentTarget;
+		e.parentNode.classList.toggle('expandido');
+	});
+}
